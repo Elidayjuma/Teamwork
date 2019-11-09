480d45962933990fdbb1f7e3d87fab7ca8522b9e
unknown <elidejuma@gmail.com>
2019-11-09 17:29:21 +0300

    Feature security password encryption

--- backend/authentication/auth.js
+++ backend/authentication/auth.js
@@ -1,14 +1,26 @@
-const db = require('../config/connection')
+const db = require('../config/connection');
+const bcrypt = require('bcrypt');
 const pool = db.pool;
 
 const createUser = async (request, response) => {
     try {
       let email = request.body.email.toLowerCase();
       let username = request.body.username.toLowerCase();
-      const {password, phone_no, first_name, last_name, avatar, job_position, department, user_address } = request.body
-  
-      const newUser = await pool.query('INSERT INTO users (username, password, email, phone_no) VALUES ($1, $2, $3, $4) RETURNING id, username, email, phone_no', [username, password, email, phone_no]);
-      const newUserDetails = await pool.query('INSERT INTO user_details (first_name, last_name, user_id, avatar, job_position, department, user_address) VALUES ($1, $2, $3, $4, $5, $6, $7)', [first_name, last_name, newUser.rows[0].id, avatar, job_position, department, user_address]);
+      const { phone_no, first_name, last_name, avatar, job_position, department, user_address } = request.body
+
+      let initialPassword = bcrypt.hashSync(phone_no.toString(), 10);
+     
+      const newUser = await pool.query(`
+        INSERT INTO users (
+        username, password, email, phone_no) 
+        VALUES ($1, $2, $3, $4) 
+        RETURNING id, username, email, phone_no`,
+        [username, initialPassword, email, phone_no]);
+      const newUserDetails = await pool.query(
+      `INSERT INTO user_details (
+        first_name, last_name, user_id, avatar, job_position, department, user_address) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7) 
+        RETURNING first_name, last_name`, [first_name, last_name, newUser.rows[0].id, avatar, job_position, department, user_address]);
       const roleUser = await pool.query('INSERT INTO role_users (user_id, role_id) VALUES ($1, $2)', [newUser.rows[0].id, 3]);
   
       const newUserResult = newUser.rows[0]
@@ -19,9 +31,11 @@ const createUser = async (request, response) => {
       })
     } catch (err) {
       console.log(err)
-      response.json({
+      response.status(500).json({
         status: "failed",
-        data: err
+        data: {
+          message: "Internal server error"
+        }
       })
     }
   }
@@ -29,17 +43,18 @@ const createUser = async (request, response) => {
 const loginUser = async (request, res) => {
     try {
       const {email, password} = request.body
-      
+     
       const doesUserExist = await pool.query('SELECT LOWER (email), password FROM users WHERE email like $1', [email])
       if (doesUserExist.rowCount<1) {
         return res.status(404).json({
           status: "failed",
           data: {
-            message: "User does not exist"
+            message: "Wrong password or email provided nonex"
           }
         })
       } else {
-        if (doesUserExist.rows[0].password === password) {
+        const passwordCheck = await bcrypt.compare(password, doesUserExist.rows[0].password );
+        if (passwordCheck == true) {
           return res.status(200).json({
             status: "success",
             data: {
@@ -47,14 +62,14 @@ const loginUser = async (request, res) => {
               user: doesUserExist
             }
           })
-        } else {
+        }else {
           return res.status(401).json({
             status: "failed",
             data: {
-              message: "Wrong password detected"
+              message: "Wrong password or email provided"
             }
           })
-        }
+        } 
       }
     } catch (err) {
         console.log (err)
